8efd3af88cbf921ae88607df615de70f402aa195
Stutommi <tommi.teetee@hotmail.com>
2019-04-16 10:13:29 +0300

    2.11_puhelin_luettelo updated to 2.18_puhelin_luettelo

--- osa_2/2.18_puhelinluettelo/src/App.js
+++ osa_2/2.18_puhelinluettelo/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import axios from "axios"
 import "./app.css"
 import Person from "./components/Person"
 import FilterField from "./components/FilterField"
 import PersonForm from "./components/PersonForm"
-import Persons from "./components/Persons"
+import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -13,25 +13,48 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then( response => setPersons(response.data
-      ))
+    personService
+      .getAll()
+      .then(persons => setPersons(persons))
   }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
-
-    if (persons.map(person => person.name).includes(newName)) {
-      return alert(`${newName} on jo luettelossa`)
-    }
 
     const newPerson = {
       name: newName,
       number: newNumber
     }
 
-    setPersons(persons.concat(newPerson))
+    if (persons.map(person => person.name).includes(newName)) {
+      const result = window.confirm(`${newName} on jo luettelossa, korvataanko vanhan numero uudella?`)
+
+      if (result) {
+        personService.update(persons.find(p => p.name === newName).id, newPerson)
+          .then(updatedPerson => {
+            setPersons(persons.map(person =>
+              person.name !== newName
+                ? person
+                : updatedPerson
+            ))
+          })
+      }
+    } else {
+      personService.create(newPerson)
+        .then(returnedPerson => setPersons(persons.concat(returnedPerson)))
+    }
+
+  }
+
+  const removePerson = (id) => () => {
+    const result = window.confirm(`Poistetaanko ${persons.find(p => p.id).name}`)
+
+    if (result) {
+      personService.remove(id)
+
+      const updatedArray = persons.filter(person => person.id !== id)
+      setPersons(updatedArray)
+    }
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
@@ -43,8 +66,9 @@ const App = () => {
 
   const showPersons = () => personsToShow.map(person =>
     <Person
-      key={person.name.toString()}
+      key={person.id}
       person={person}
+      handleClick={removePerson(person.id)}
     />)
 
   return (
--- osa_2/2.18_puhelinluettelo/src/components/Person.js
+++ osa_2/2.18_puhelinluettelo/src/components/Person.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Person = ({ person }) =>
+const Person = ({ person, handleClick }) =>
     <p>
         {person.name} {person.number}
+        <button onClick={handleClick}>poista</button>
     </p>
 
 export default Person
